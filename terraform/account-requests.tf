95d03c5c3668cf8bd994bfa72bc39a0a5b63a3f0
Gandhi <vgn@3c0630478548.ant.amazon.com>
2022-10-06 14:18:56 +1100

    vgn-acct23 creation

---
+++ terraform/account-requests.tf
@@ -0,0 +1,35 @@
+module "account_request_01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "vgn+23@amazon.com"
+    AccountName               = "vgn-acct23"
+    ManagedOrganizationalUnit = "Sandbox" 
+    SSOUserEmail              = "vgn@amazon.com"
+    SSOUserFirstName          = "vgn-acct23"
+    SSOUserLastName           = "user"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "vgn@amazon.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "test"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
