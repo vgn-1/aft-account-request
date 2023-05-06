ef424d663d97ddaa2b3cc35c3719453137d3a201
vgn-1 <109327302+vgn-1@users.noreply.github.com>
2023-05-06 13:37:26 +1000

    Update account-requests.tf

--- terraform/account-requests.tf
+++ terraform/account-requests.tf
@@ -2,11 +2,11 @@ module "account_request_01" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "vgn+25@amazon.com"
-    AccountName               = "vgn-acct25"
+    AccountEmail              = "vgn+24@amazon.com"
+    AccountName               = "vgn-acct24"
     ManagedOrganizationalUnit = "Sandbox" 
     SSOUserEmail              = "vgn@amazon.com"
-    SSOUserFirstName          = "vgn-acct25"
+    SSOUserFirstName          = "vgn-acct24"
     SSOUserLastName           = "user"
   }
 
