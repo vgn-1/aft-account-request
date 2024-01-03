1c66ba11b1226a0e596248ca0352a233ca934baf
vgn-1 <vgn@amazon.com>
2024-01-03 13:24:04 +1100

    Update account-requests.tf to add mgmt acct to AFT

--- terraform/account-requests.tf
+++ terraform/account-requests.tf
@@ -2,11 +2,11 @@ module "account_request_01" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "vgn+21@amazon.com"
-    AccountName               = "vgn-acct21"
+    AccountEmail              = "vgn+23@amazon.com"
+    AccountName               = "vgn-acct23"
     ManagedOrganizationalUnit = "Sandbox" 
     SSOUserEmail              = "vgn@amazon.com"
-    SSOUserFirstName          = "vgn-acct21"
+    SSOUserFirstName          = "vgn-acct23"
     SSOUserLastName           = "user"
   }
 
@@ -33,3 +33,39 @@ module "account_request_01" {
 
   account_customizations_name = "SANDBOX"
 }
+
+module "account_request_02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "vgn@amazon.com"
+    AccountName               = "vgn"
+    ManagedOrganizationalUnit = "Root" 
+    SSOUserEmail              = "vgn@amazon.com"
+    SSOUserFirstName          = "AWS Control Tower"
+    SSOUserLastName           = "Admin"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "vgn@amazon.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "test"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+  
+}
+
